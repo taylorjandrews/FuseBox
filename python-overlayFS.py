6adc9ef6ffaea769954aae3109c58062a740d057
Taylor Andrews <taylor.andrews-1@colorado.edu>
2014-06-09 14:42:21 -0600

    saved changes to overlay-FS

--- python-overlayFS.py
+++ python-overlayFS.py
@@ -1,17 +1,68 @@
 #!/usr/bin/env python
 
-# step-1
-# will mount an FS, but useless beyond that
+import os, sys
+from errno import *
+from stat import *
+import fcntl
 
 import fuse
 
-fuse.fuse_python_api = ( 0, 2 )
+fuse.fuse_python_api = (0, 2)
 
-class ExampleFS( fuse.Fuse ):
-    def __init__( self, *args, **kw ):
-    fuse.Fuse.__init__( self, *args, **kw )
+class ENCFS(Fuse):
 
-if __name__ == '__main__':
-    examplefs = ExampleFS()
-    args = examplefs.parse( errex = 1 )
-    examplefs.main()
+    def getattr(self, path):
+        return os.lstat("." + path)
+
+    def readlink(self, path):
+        return os.readlink("." + path)
+
+    def readdir(self, path, offset):
+        for e in os.listdir("." + path):
+            yield fuse.Direntry(e)
+
+    def unlink(self, path):
+        os.unlink("." + path)
+
+    def rmdir(self, path):
+        os.rmdir("." + path)
+
+    def symlink(self, path, path1):
+        os.symlink(path, "." + path1)
+
+    def rename(self, path, path1):
+        os.rename("." + path, "." + path1)
+
+    def link(self, path, path1):
+        os.link("." + path, "." + path1)
+
+    def chmod(self, path, mode):
+        os.chmod("." + path, mode)
+
+    def chown(self, path, user, group):
+        os.chown("." + path, user, group)
+
+    def truncate(self, path, len):
+        f = open("." + path, "a")
+        f.truncate(len)
+        f.close()
+
+    def mknod(self, path, mode, dev):
+        os.mknod("." + path, mode, dev)
+
+    def mkdir(self, path, mode):
+        os.mkdir("." + path, mode)
+
+    def utime(self, path, times):
+        os.utime("." + path, times)
+
+    def access(self, path, mode):
+        if not os.access("." + path, mode):
+            return -EACCES
+
+    def statfs(self):
+        return os.statvfs(".")
+
+    def fsinit(self):
+        os.chdir(self.root)
+
