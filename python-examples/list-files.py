e35b975beee4e16c5e8f19f02d921c0ac5eb0ed5
Taylor Andrews <taylor.andrews-1@colorado.edu>
2014-06-30 11:36:28 -0600

    added list-files.py

---
+++ python-examples/list-files.py
@@ -0,0 +1,25 @@
+import dropbox
+
+appinfo = open("appkey.txt",'r')
+APP_KEY = appinfo.readline().strip()
+APP_SECRET = appinfo.readline().strip()
+
+flow = dropbox.client.DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
+
+authorize_url = flow.start()
+
+authorize_url = flow.start()
+print '1. Go to: ' + authorize_url
+print '2. Click "Allow" (you might have to log in first)'
+print '3. Copy the authorization code.'
+code = raw_input("Enter the authorization code here: ").strip()
+
+access_token, user_id = flow.finish(code)
+
+client = dropbox.client.DropboxClient(access_token)
+print 'linked account: ', client.account_info()
+
+
+folder_metadata = client.metadata('/')
+for e in folder_metadata.contents:
+    print e.fileName
