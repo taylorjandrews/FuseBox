12cfa2fcff8f32c26d0dffc1b1cd97730f54ef39
Taylor Andrews <taylor.andrews-1@colorado.edu>
2014-07-07 10:53:13 -0600

    added oauth bypass

--- python-examples/list-files.py
+++ python-examples/list-files.py
@@ -3,21 +3,13 @@ import dropbox
 appinfo = open("appkey.txt",'r')
 APP_KEY = appinfo.readline().strip()
 APP_SECRET = appinfo.readline().strip()
+ACCESS_TOKEN = appinfo.readline().strip()
 
-flow = dropbox.client.DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
+client = dropbox.client.DropboxClient(ACCESS_TOKEN)
 
-authorize_url = flow.start()
+folder_metadata = client.metadata('/Custos')
 
-authorize_url = flow.start()
-print '1. Go to: ' + authorize_url
-print '2. Click "Allow" (you might have to log in first)'
-print '3. Copy the authorization code.'
-code = raw_input("Enter the authorization code here: ").strip()
-
-access_token, user_id = flow.finish(code)
-
-client = dropbox.client.DropboxClient(access_token)
-
-folder_metadata = client.metadata('/CSCI1300')
 for entry in folder_metadata['contents']:
-    print(entry['path'])
+    entry['path'] = entry['path'].split('/')
+    filename = entry['path'][len(entry['path'])-1]
+    print(filename)
