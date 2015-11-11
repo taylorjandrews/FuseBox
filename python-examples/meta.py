0ff5d371b375bda80ea2341f5cabc76ce34032c8
Taylor Andrews <taylor.andrews-1@colorado.edu>
2015-11-11 00:57:48 -0700

    Wrote json example

---
+++ python-examples/meta.py
@@ -0,0 +1,33 @@
+import json
+
+f = open("t.txt", 'rb+')
+
+decoder = json.JSONDecoder()
+
+def get_decoded_and_remainder(input_data):
+    obj, end = decoder.raw_decode(input_data)
+    remaining = input_data[end:]
+    return (obj, end, remaining)
+
+encoded_object = '[{"padding_size": 13, "other_field": "temp_data"}]'
+extra_text = "This text is not JSON."
+
+f.seek(0)
+
+print 'JSON first:'
+metadata, metasize, dataobj = get_decoded_and_remainder(f.read())
+filedata = repr(dataobj)
+print 'Object              :', metadata
+print 'End of parsed input :', metasize
+print 'Remaining text      :', filedata
+
+m = json.loads(json.dumps(metadata[0]))
+m["padding_size"] = 10
+
+metadata[0] = m
+
+f.seek(0)
+f.write(json.dumps(metadata))
+f.write(filedata[1:-1])
+
+f.close()
